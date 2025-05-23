4a62751ca00418396037754f2536d606fe3b6281
unknown <daniel.ruge-g@labinfo.is.escuelaing.edu.co>
2025-05-23 16:51:20 -0500

    endpoint de creación de cita

--- src/main/java/com/example/demo/model/Cita.java
+++ src/main/java/com/example/demo/model/Cita.java
@@ -1,11 +1,13 @@
 package com.example.demo.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-
+@Document(collection = "attendance")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
